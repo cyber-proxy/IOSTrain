523b216e26e6c70c351b8a938a10f69a9640d6d5
tsotumu <tsotumu@163.com>
2020-05-14 15:25:18 +0800

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 李成 on 2020/5/14.
+//  Copyright © 2020 李成. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
